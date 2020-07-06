4d4901ebe4b59fc661424b496fa62fc7c07877ae
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2020-07-06 13:47:04 -0700

    implemented basic up to showing a goat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  readonly NUM_DOORS:number = 3;
+  readonly CAR:string = "car";
+  readonly GOAT:string = "goat";
+  carPosition:number;
+  prizes:string[] = new Array(this.NUM_DOORS);
+  choice:number;
+  doorToShow:number;
+
+  ngOnInit() {
+    this.carPosition = Math.floor(Math.random() * this.NUM_DOORS);
+    for (let i = 0; i < this.NUM_DOORS; i++) {
+      this.prizes[i] = i == this.carPosition? this.CAR : this.GOAT;
+    }
+    console.log(this.prizes);
+  }
+
+  constructor() {}
+  
+  onClickDoor = (e:any):void => {
+    let id = e.target.dataset.id;
+    this.choice = parseInt(id);
+  }
+
+  cancelChoice = ():void => {
+    this.choice = null;
+  }
+
+  showGoat = ():void => {
+    let goatIndex:number[] = [];
+    this.prizes.forEach((prize, index) => {
+      if (prize == this.GOAT && index != this.choice) {
+        goatIndex.push(index);
+      }
+    });
+
+    this.doorToShow = goatIndex[Math.floor(Math.random() * goatIndex.length)];
+    console.log("door to show: " + this.doorToShow);
+  }
 }
